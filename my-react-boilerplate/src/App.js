6cd5098a485bbf213fecc9ae5bcb460de4765704
jicibils <jicibils@hotmail.com>
2019-06-14 01:31:29 -0300

    Add Webpack config and Babel

---
+++ my-react-boilerplate/src/App.js
@@ -0,0 +1,19 @@
+// The default component for create-react-app
+import React from 'react';
+// import Root from './routes/Root';
+// import { Provider } from 'react-redux';
+// import store from './redux/createStore';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello Cruel World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
